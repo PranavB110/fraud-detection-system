ab9c5d15dee07015e381984455cde0321f71291a
Pranav Bhakare <pranavbhakare38@gmail.com>
2026-03-22 23:25:34 +0530

    Update API URL to EC2 backend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -25,7 +25,7 @@ function App() {
     setLoading(true)
 
     try {
-      const response = await fetch('http://localhost:8000/predict', {
+      const response = await fetch('http://43.205.192.243:8000/predict', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ features: values })
